6a2ffec7d5919f0136a82a1e20b7f38525b7a7bd
snehadeep bhowmick <snehadeepbhowmick@100215.local>
2017-11-03 16:12:53 +0530

    Added Enter keypress for unsupervised installation

--- galieli-netdata-installer/linux-netdata_installer.bash
+++ galieli-netdata-installer/linux-netdata_installer.bash
@@ -22,5 +22,5 @@ yum install -y autoconf automake curl gcc git libmnl-devel libuuid-devel lm_sens
 #git clone https://github.com/seamless-distribution-systems/galilei.git --depth=1
 cd netdata
 echo "Working from `pwd`"
-`pwd`/netdata-installer.sh
+echo -ne '\n' | `pwd`/netdata-installer.sh
 echo "Bye!!!"
